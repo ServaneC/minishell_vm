e717ecaf915e7daa45cab18312d321e2faece0fe
ServaneC <schene@student.42.fr>
2020-06-07 14:23:16 +0200

    norm cd

--- srcs/builtin_cd.c
+++ srcs/builtin_cd.c
@@ -6,7 +6,7 @@
 /*   By: schene <schene@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/27 15:40:08 by schene            #+#    #+#             */
-/*   Updated: 2020/06/07 13:41:08 by schene           ###   ########.fr       */
+/*   Updated: 2020/06/07 14:22:10 by schene           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,37 +33,39 @@ static int		change_value(t_list *env, char *name, char *newvalue)
 	return (0);
 }
 
-void			change_dir(t_data *data, char *path)
+static void		cd_from_rm(t_data *data)
+{
+	char	*str;
+
+	str = ft_strdup("cd");
+	ft_error(&str);
+	str = NULL;
+	change_value(data->env, "OLDPWD", var_value(data->env, "$PWD"));
+	str = ft_strjoin(var_value(data->env, "$PWD"), "/.");
+	change_value(data->env, "PWD", str);
+	free(str);
+	str = NULL;
+}
+
+static void		change_dir(t_data *data, char *path)
 {
 	char			old_pwd[MAX_PATH];
 	char			pwd[MAX_PATH];
 	struct stat		*buf;
 	char			*str;
-	
 
-	if (getcwd(old_pwd, MAX_PATH) == NULL && data->cmd[1][0] == '.' && !data->cmd[1][1])
-	{
-		str = ft_strdup("cd");
-		ft_error(&str);
-		str = NULL;
-		change_value(data->env, "OLDPWD", var_value(data->env, "$PWD"));
-		str = ft_strjoin(var_value(data->env, "$PWD"), "/.");
-		change_value(data->env, "PWD", str);
-		free(str);
-		return ;
-	}
+	if (getcwd(old_pwd, MAX_PATH) == NULL &&
+		data->cmd[1][0] == '.' && !data->cmd[1][1])
+		return (cd_from_rm(data));
 	buf = (struct stat *)malloc(sizeof(struct stat));
-	if (lstat(path, buf) == 0)
+	if (lstat(path, buf) == 0 && chdir(path) == 0)
 	{
-		if (chdir(path) == 0)
-		{
-			getcwd(pwd, MAX_PATH);
-			getcwd(old_pwd, MAX_PATH);
-			change_value(data->env, "PWD", pwd);
-			free(data->dir);
-			data->dir = ft_strdup(pwd);
-			change_value(data->env, "OLDPWD", old_pwd);
-		}
+		getcwd(pwd, MAX_PATH);
+		getcwd(old_pwd, MAX_PATH);
+		change_value(data->env, "PWD", pwd);
+		free(data->dir);
+		data->dir = ft_strdup(pwd);
+		change_value(data->env, "OLDPWD", old_pwd);
 	}
 	else
 	{
