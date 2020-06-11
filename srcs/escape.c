b452aa31ad23ff29c6f5cfc0d3e0705c9fcfc846
ServaneC <schene@student.42.fr>
2020-06-11 14:39:48 +0200

    bug

--- srcs/escape.c
+++ srcs/escape.c
@@ -6,7 +6,7 @@
 /*   By: schene <schene@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/09 11:19:54 by schene            #+#    #+#             */
-/*   Updated: 2020/06/11 13:58:50 by schene           ###   ########.fr       */
+/*   Updated: 2020/06/11 14:39:17 by schene           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,13 +36,13 @@ static int	tmp_is_quote(int i, char **tmp, char *cmd)
 		{
 			if (cmd[i] == '\\' && cmd[i + 1] && cmd[i + 1] == '\"')
 				i++;
-			else if (cmd[i] == '\\' &&
-				((cmd[i + 1] && cmd[i + 1] != '$') || !cmd[i + 1]))
+			else if (cmd[i] == '\\' && ((cmd[i + 1] && cmd[i + 1] != '$'
+				&& cmd[i + 1] != '\\') || !cmd[i + 1]))
 				*tmp = clean_ft_strjoin(*tmp, ft_strdup("\\"));
 			*tmp = clean_ft_strjoin(*tmp, ft_substr(cmd, i, 1));
 			if (cmd[i + 1] &&
 				cmd[i + 1] == '\\' && cmd[i + 2] && cmd[i + 2] == '\"')
-				i++;
+				*tmp = clean_ft_strjoin(*tmp, ft_substr(cmd, ++i, 1));
 		}
 		else if (cmd[++i])
 			*tmp = clean_ft_strjoin(*tmp, ft_substr(cmd, i, 1));
